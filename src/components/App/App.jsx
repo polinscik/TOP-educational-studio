faf96e62a10b2155f79ee6bda0867cdcdbd14d18
Polina <polinscik@gmail.com>
2024-09-17 21:17:14 +0300

    add folder structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+// import {useState} from "react";
+import "./App.scss";
+import SliderLanding from "../SliderLanding/SliderLanding";
+
+function App() {
+  return (
+    <>
+      <div>App</div>
+      <SliderLanding></SliderLanding>
+    </>
+  );
+}
+
+export default App;
