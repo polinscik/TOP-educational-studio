85ff1e59bb6ac1a5c5631b4ec9fc9541e67aeb68
Polina <polinscik@gmail.com>
2024-09-21 17:17:38 +0300

    add component style

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import "./MainPage.scss";
+import {Fragment} from "react";
+import SliderLanding from "../SliderLanding/SliderLanding";
+
+export default function MainPage() {
+  return (
+    <Fragment>
+      <SliderLanding></SliderLanding>
+    </Fragment>
+  );
+}
