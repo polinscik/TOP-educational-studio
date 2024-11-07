d25813e1033fe83fb64e90b915f29a95d9b677e0
Emilia <emiliya.egorova.2018@mail.ru>
2024-11-07 21:05:31 +0300

    deploy again

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,14 +1,31 @@
+import { useState, useEffect } from "react";
 import "./MainPage.scss";
-import {Fragment} from "react";
+import { Fragment } from "react";
 import SliderLanding from "../SliderLanding/SliderLanding";
 import ReviewSlider from "../ReviewSlider/ReviewSlider";
 import TeachersCards from "../TeachersCards/TeachersCards.jsx";
 import CourseCards from "../CourseCards/CourseCards";
+import Form from "../Form/Form.jsx";
 
 export default function MainPage() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div style={{backgroundColor: "#0f1012"}}>
+    <div style={{ backgroundColor: "#0f1012" }}>
       <SliderLanding></SliderLanding>
+      {windowWidth < 1200 && <Form />}
       <CourseCards></CourseCards>
       <TeachersCards />
       <ReviewSlider />
