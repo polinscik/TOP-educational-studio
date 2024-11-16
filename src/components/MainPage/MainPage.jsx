ba29900cc31feb405eac225e0c50913803c9ec04
Polina <polinscik@gmail.com>
2024-11-16 16:24:22 +0300

    fix font sizes

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import "./MainPage.scss";
-import { Fragment } from "react";
+import {Fragment} from "react";
 import SliderLanding from "../SliderLanding/SliderLanding";
 import ReviewSlider from "../ReviewSlider/ReviewSlider";
 import TeachersCards from "../TeachersCards/TeachersCards.jsx";
@@ -15,15 +15,15 @@ export default function MainPage() {
       setWindowWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <div style={{ backgroundColor: "#0f1012" }}>
+    <div style={{backgroundColor: "#0f1012"}}>
       <SliderLanding></SliderLanding>
       {windowWidth < 1200 && <Form />}
       <CourseCards></CourseCards>
