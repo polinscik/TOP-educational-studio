487707c50c5ae3512d40d425923d65d27b529579
Emilia <emiliya.egorova.2018@mail.ru>
2024-11-28 01:53:37 +0300

    fix: scss ifor map iframe

--- src/components/OurContacts/OurContacts.jsx
+++ src/components/OurContacts/OurContacts.jsx
@@ -11,7 +11,7 @@ const OurContacts = () => {
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2441.8758771693865!2d104.25448307627784!3d52.26379897199517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5da823573d93115d%3A0xecb8332561174cd9!2z0YPQuy4g0JPRgNC40LHQvtC10LTQvtCy0LAsIDEwOCwg0JjRgNC60YPRgtGB0LosINCY0YDQutGD0YLRgdC60LDRjyDQvtCx0LsuLCA2NjQwNzQ!5e0!3m2!1sru!2sru!4v1732485708541!5m2!1sru!2sru"
                         width="560"
                         height="448"
-                        style={{ border: 0 }}
+                        style={{ border: 0, borderRadius: '0.4rem' }} 
                         allowFullScreen=""
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
